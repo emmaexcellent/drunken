5459dd3b0a4df7fd52b71de03a80a684fb848653
Emmaexcellent <emmaexcellent030@gmail.com>
2024-12-18 03:56:46 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" prefers-color-scheme="light">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
